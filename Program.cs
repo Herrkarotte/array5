7bfe9ae96eb6ff890f9b17dc550b75685f536a47
Herr Karotte <dvp03ne@yandex.ru>
2023-10-19 00:31:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace array5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int n = 3;
+            Random rnd = new Random();
+            int[] Arr1 = new int[n * n];
+            int[] Arr2 = new int[n * n];
+            int[] Arr3 = new int[n * n];
+            for (int i = 0; i != n * n; i++)
+            {
+                Arr1[i] = i + 1;
+                Arr2[i] = i + 1;
+            }
+
+            for (int i = 0; i != n; i ++)
+            {
+                for (int j = 0; j != n; j++)
+                {
+                    for (int k = 0; k != n; k++)
+                    {
+                        Arr3[i * n + j] += Arr1[i * n + k] * Arr2[k * n + j];
+                    }
+                }
+            }
+            for (int i = 0; i != n; i++)
+            {
+                for (int j = 0; j != n; j++)
+                {
+                    Console.Write($"{Arr3[i * n + j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
